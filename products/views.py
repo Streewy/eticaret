8a35041c25180b3d13e3f5c654688060917c2d21
Streewy <cse.canerbostanci@gmail.com>
2018-04-04 12:03:55 +0300

    pycharm git2

--- products/views.py
+++ products/views.py
@@ -4,19 +4,23 @@ from products.models import *
 # Create your views here.
 
 def index(request):
-    main_categories = MainCategory.objects.all()
-    sub_categories = SubCategory.objects.all()
+    main_categories = MainCategory.objects.all().order_by('title')
+    sub_categories = SubCategory.objects.all().order_by('title')
     products = Product.objects.all()
 
     return render(request, 'index.html', locals())
 
 def mainCategory(request, url):
     mainCategories = MainCategory.objects.filter(slug=url)
+    main_categories = MainCategory.objects.all().order_by('title')
+    sub_categories = SubCategory.objects.all().order_by('title')
 
     return render(request, 'main.html', locals())
 
 def subCategory(request, mainurl, suburl):
     subCategories = Product.objects.filter(sub_category__slug=suburl, sub_category__main_category__slug=mainurl)
+    main_categories = MainCategory.objects.all().order_by('title')
+    sub_categories = SubCategory.objects.all().order_by('title')
 
     return render(request, 'sub.html', locals())
 
